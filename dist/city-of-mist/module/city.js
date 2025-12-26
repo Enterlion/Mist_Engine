412b354c0f5066bb6b87a0b12a68d88310a433b2
Enterlion <clenterline@gmail.com>
2025-12-26 18:08:12 -0500

    Fixed build process and removed paper texture background

---
+++ dist/city-of-mist/module/city.js
@@ -0,0 +1,138 @@
+/**
+ * City of Mist System
+ * Author: Taragnor
+ */
+// Import Modules
+// Note: Must initialize systemModule before BaseSystemModule or its derived classes for some reason to avoid error
+import { LitMSystem } from "./systemModule/litm-system.js";
+import { SystemModule } from "./config/system-module.js";
+import { BaseSystemModule } from "./systemModule/baseSystemModule.js";
+import { MistEngineSystem } from "./systemModule/mist-engine.js";
+import { CoMTypeSystem } from "./systemModule/com-type-system.js";
+import { OtherscapeSystem } from "./systemModule/otherscape.js";
+import { CoMSystem } from "./systemModule/com-system.js";
+import { MistChatMessage } from "./mist-chat-message.js";
+import { MistRoll } from "./mist-roll.js";
+import { CityDataMigration } from "./migration.js";
+import { ACTORMODELS } from "./datamodel/actor-types.js";
+import { ITEMMODELS } from "./datamodel/item-types.js";
+import { TokenTooltip } from "./token-tooltip/token-tooltip.js";
+import { preloadHandlebarsTemplates } from "./city-templates.js";
+import { CityRoll } from "./city-roll.js";
+import { CityHelpers } from "./city-helpers.js";
+import { CityActor } from "./city-actor.js";
+import { CityItem } from "./city-item.js";
+import { CityItemSheet, CityItemSheetSmall, CityItemSheetLarge } from "./city-item-sheet.js";
+import { CityCrewSheet } from "./city-crew-sheet.js";
+import { CityThreatSheet } from "./city-threat-sheet.js";
+import { CityCharacterSheet } from "./city-character-sheet.js";
+import { registerSystemSettings } from "./settings.js";
+import { StatusTrackerWindow } from "./city-status-tracker/city-status-tracker.js";
+import { EnhancedActorDirectory } from "./enhanced-directory/enhanced-directory.js";
+import { CityHandlebarsHelpers } from "./city-handlebars-helpers.js";
+import { CitySockets } from "./city-sockets.js";
+import { DragAndDrop } from "./dragAndDrop.js";
+import { CityKeyBinds } from "./keybindings.js";
+import { ClueChatCards } from "./clue-cards.js";
+import { StoryTagWindow } from "./story-tag-application.js";
+/* -------------------------------------------- */
+/*  Foundry VTT Initialization                  */
+/* -------------------------------------------- */
+console.log("COM preinit");
+export function localize(str) {
+    if (!game?.i18n?.localize) {
+        console.log("Can't localize too early");
+        return "";
+    }
+    return game.i18n.localize(str);
+}
+Hooks.on('renderChatMessage', (app, html, data) => CityRoll.diceModListeners(app, html, data));
+Hooks.on('renderChatMessage', (app, html, data) => CityRoll.showEditButton(app, html, data));
+Hooks.on('renderChatMessage', (_app, html, _data) => DragAndDrop.addDragFunctionality(html));
+Hooks.on('renderChatMessage', (app, html, data) => ClueChatCards.clueEditButtonHandlers(app, html, data));
+Hooks.on('ready', async () => {
+    CitySockets.init();
+    // window.CitySockets = CitySockets;
+});
+Hooks.once("cityDBLoaded", async function () {
+    CityHelpers.applyColorization();
+    return true;
+});
+Hooks.on("registerRulesSystemPhase", (sys) => {
+    sys.registerRulesSystem(new CoMSystem());
+    sys.registerRulesSystem(new OtherscapeSystem());
+    sys.registerRulesSystem(new LitMSystem());
+});
+function registerDataModels() {
+    CONFIG.Actor.dataModels = ACTORMODELS;
+    CONFIG.Item.dataModels = ITEMMODELS;
+    CONFIG.Dice.rolls = [MistRoll];
+    //@ts-ignore
+    CONFIG.ChatMessage.documentClass = MistChatMessage;
+}
+Hooks.once("ready", async () => CityHelpers.cacheSounds());
+Hooks.once("init", async function () {
+    console.log(`***********************************`);
+    console.log(`Initializing City of Mist System`);
+    console.log(`***********************************`);
+    registerDataModels();
+    await SystemModule.init();
+    registerSystemSettings();
+    await SystemModule.active.activate();
+    game.city = {
+        CityActor,
+        CityItem,
+        BaseSystemModule,
+        CoMTypeSystem,
+        MistEngineSystem,
+        ActorSheets: [CityCharacterSheet, CityThreatSheet, CityCrewSheet],
+        ItemSheets: [CityItemSheet, CityItemSheetLarge, CityItemSheetSmall],
+    };
+    CONFIG.Item.documentClass = CityItem;
+    CONFIG.Actor.documentClass = CityActor;
+    preloadHandlebarsTemplates();
+    if (game.settings.get("city-of-mist", "enhancedActorDirectory")) {
+        EnhancedActorDirectory.init();
+    }
+    CityHandlebarsHelpers.init();
+    StoryTagWindow.init();
+    // StoryTagDisplayContainer.init();
+    CityKeyBinds.init();
+    TokenTooltip.init();
+});
+Hooks.on("cityDBLoaded", async () => {
+    await CityDataMigration.checkMigration();
+});
+//Support for TaragnorSecurity module if installed
+//@ts-ignore
+Hooks.on("encryptionPreEnable", (taragnorSec) => {
+    taragnorSec.setEncryptable(CityActor, [CityCharacterSheet, CityThreatSheet, CityCrewSheet], ["system.gmnotes", "system.mythos"]);
+    taragnorSec.setEncryptable(CityItem, [CityItemSheetLarge], ["system.description"]);
+    console.log("Taragnor Security: CoM Encryption support enabled");
+});
+/* City of Mist Status Tracker */
+Hooks.on("renderJournalDirectory", async () => {
+    StatusTrackerWindow.init();
+    // window.statusTrackerWindow = new StatusTrackerWindow();
+});
+Hooks.on("getSceneControlButtons", function (controls) {
+    //disabling status tracker as V13 didn't like it 
+    //TODO: fix later, not sure how many people use this feature
+    // let tileControls = controls.find((x:any) => x.name === "tokens");
+    // if (game.user.isGM){
+    // 	tileControls.tools.push({
+    // 		icon: "fas fa-medkit",
+    // 		name: "city-of-mist-status-tracker",
+    // 		title: game.i18n.localize("CityOfMistTracker.trackerwindow.title"),
+    // 		button: true,
+    // 		onClick: () => StatusTrackerWindow._instance.render(true)
+    // 	});
+    // }
+});
+Hooks.on("renderApplication", function () {
+    if (StatusTrackerWindow._instance) {
+        StatusTrackerWindow._instance.render(false);
+    }
+});
+//@ts-ignore
+window.CityHelpers = CityHelpers;
---
+++ dist/city-of-mist/module/story-tag-application.js
@@ -0,0 +1,295 @@
+import { CityActor } from "./city-actor.js";
+import { CityHelpers } from "./city-helpers.js";
+import { localize } from "./city.js";
+import { DragAndDrop } from "./dragAndDrop.js";
+import { SceneTags } from "./scene-tags.js";
+import { CitySettings } from "./settings.js";
+import { HTMLTools } from "./tools/HTMLTools.js";
+import { HTMLHandlers } from "./universal-html-handlers.js";
+export class StoryTagWindow extends Application {
+    static instance;
+    static init() {
+        this.instance = new StoryTagWindow();
+        const location = CitySettings.sceneTagWindowPosition();
+        if (location == "hide")
+            return;
+        Hooks.on("ready", () => {
+            let top, left;
+            const doc = $(document);
+            switch (location) {
+                case "left":
+                    left = 50 + doc.find("#ui-left-column-1").width();
+                    top = 50 + doc.find("#scene-navigation-active").height();
+                    break;
+                case "right":
+                    left = (-350) + doc.find("#ui-right").position().left;
+                    top = 50 + doc.find("#navigation").height();
+                    break;
+            }
+            this.instance.render(true);
+            //not sure why it won't work normally but it seems to resist movement if done immediately
+            setTimeout(() => this.instance.setPosition({ left, top }), 500);
+        });
+    }
+    get title() {
+        return localize("CityOfMist.settings.sceneTagWindow.name");
+    }
+    constructor() {
+        super();
+        this.initHooks();
+    }
+    static get defaultOptions() {
+        return foundry.utils.mergeObject(super.defaultOptions, {
+            classes: ["story-tag-window", "scene-tag-window"],
+            template: "systems/city-of-mist/templates/story-tag-window.hbs",
+            width: 400,
+            height: 500,
+            // tabs: []
+        });
+    }
+    _getHeaderButtons() {
+        const buttons = [
+        // {
+        //   label: "Close",
+        //   class: "close",
+        //   icon: "fa-solid fa-xmark",
+        //   onclick: () => this.close()
+        // }
+        ];
+        return buttons;
+    }
+    initHooks() {
+        Hooks.on("updateActor", () => this.refreshContents());
+        Hooks.on("updateItem", () => this.refreshContents());
+        Hooks.on("createItem", () => this.refreshContents());
+        Hooks.on("createActor", () => this.refreshContents());
+        Hooks.on("deleteItem", () => this.refreshContents());
+        Hooks.on("deleteActor", () => this.refreshContents());
+        Hooks.on("updateSceneTags", async () => this.refreshContents());
+        Hooks.on("TagOrStatusSelected", () => { this.refreshContents(); return true; });
+        Hooks.on("TagOrStatusSelectChange", () => this.refreshContents());
+        Hooks.on("createCombatant", () => this.refreshContents());
+        Hooks.on("deleteCombatant", () => this.refreshContents());
+    }
+    activateListeners(html) {
+        HTMLHandlers.applyBasicHandlers(html, false);
+        html.find(".create-story-tag").on("click", this.createStoryTag);
+        html.find(".create-status").on("click", this.createStatus);
+        html.find(".combatant-name").on("click", this.centerOnToken);
+        html.find(".combatant-name").rightclick(this.openSheet);
+        html.find(".scene-tags-block").on("drop", this._dragDropEvent.bind(this));
+        html.find(".scene-tags-block .status").on("drop", this._dragDropEvent.bind(this));
+        html.find(".scene-tags-block").on("drop", this._dragDropEvent.bind(this));
+        html.find(".combatant-block").on("drop", this._dragDropEvent.bind(this));
+        html.find(".combatant-block .status").on("drop", this._dropOnOtherStatus.bind(this));
+        $(this.element).find(".toggle-combat").on("click", ev => CityHelpers.toggleCombat(ev));
+        this.initDraggability();
+    }
+    initDraggability() {
+        const html = $(this.element);
+        const drag = new foundry.applications.ux.Draggable.implementation(this, html, false, true);
+        drag._onDragMouseMove = function _newOnDragMouseMove(event) {
+            event.preventDefault();
+            this.app.setPosition({
+                left: this.position.left + (event.clientX - this._initial.x),
+                top: this.position.top + (event.clientY - this._initial.y),
+            });
+        };
+    }
+    refreshContents() {
+        if (CitySettings.sceneTagWindowPosition() == "hide") {
+            this.element.hide();
+            return;
+            ;
+        }
+        this.element.show();
+        // console.log("Refreshing Contents");
+        this.render(false);
+    }
+    async getData() {
+        const data = super.getData();
+        const tagsAndStatuses = await SceneTags.getSceneTagsAndStatuses();
+        const combat = game.combats.find(comb => comb.scene == game.scenes.current) || game.combat;
+        const combatants = (combat ? combat.combatants.contents : [])
+            .filter(combatant => {
+            if (!combatant.actor || !combatant.token)
+                return false;
+            if (CityHelpers.sceneTagWindowFilterEmpty())
+                return combatant.actor.storyTagsAndStatuses.length > 0;
+            else
+                return true;
+        });
+        const combatActors = combatants.map(x => x.actor);
+        const showcasedActors = game.actors.contents
+            .filter(actor => !combatActors.includes(actor)
+            && actor.items.contents.some(item => item.isShowcased));
+        const combatActorsSize = combatActors.reduce((acc, x) => {
+            return acc + 2 + x.storyTagsAndStatuses.length;
+        }, 3);
+        const showcasedActorsSize = showcasedActors.reduce((acc, x) => {
+            return acc + 2 + x.storyTagsAndStatuses
+                .filter(item => item.isShowcased)
+                .length;
+        }, 3);
+        const shrink = (combatActorsSize + showcasedActorsSize) > 50;
+        const templateData = {
+            tagsAndStatuses,
+            combatants,
+            showcasedActors,
+            shrink
+        };
+        return {
+            ...data,
+            ...templateData,
+        };
+    }
+    getTokenAt(event) {
+        const ownerId = String(HTMLTools.getClosestDataNT(event, "ownerId", ""));
+        if (ownerId.length < 1) {
+            return SceneTags;
+        }
+        const tokenId = String(HTMLTools.getClosestDataNT(event, "tokenId", ""));
+        const sceneId = String(HTMLTools.getClosestDataNT(event, "sceneId", ""));
+        const x = CityHelpers.getOwner(ownerId, tokenId, sceneId);
+        if (x instanceof CityActor) {
+            return x;
+        }
+        else {
+            return undefined;
+        }
+    }
+    getStatusAt(event) {
+        const ownerId = String(HTMLTools.getClosestDataNT(event, "ownerId", ""));
+        const statusId = String(HTMLTools.getClosestDataNT(event, "statusId", ""));
+        const tokenId = String(HTMLTools.getClosestDataNT(event, "tokenId", ""));
+        const sceneId = String(HTMLTools.getClosestDataNT(event, "sceneId", ""));
+        if (!statusId || !ownerId)
+            return undefined;
+        const owner = CityHelpers.getOwner(ownerId, tokenId, sceneId);
+        return owner.items.get(statusId);
+    }
+    async toggleVisibility() {
+        const element = $(this.element);
+        if (element.css('visibility') == 'hidden') {
+            element.css('visibility', 'visible');
+        }
+        else {
+            element.css('visibility', 'hidden');
+        }
+    }
+    static async toggleVisibility() {
+        this.instance.toggleVisibility();
+    }
+    async createStoryTag(event) {
+        //somewhat hacky code with the exception as a branch
+        try {
+            const ownerId = HTMLTools.getClosestData(event, "ownerId");
+            const tokenId = HTMLTools.getClosestData(event, "tokenId");
+            const sceneId = HTMLTools.getClosestData(event, "sceneId");
+            CityHelpers.getOwner(ownerId, tokenId, sceneId);
+        }
+        catch (e) {
+            console.log("Creating story Tag");
+            console.log(e);
+            return await SceneTags.createSceneTag();
+        }
+        return await HTMLHandlers.createStoryTag(event);
+    }
+    async createStatus(event) {
+        //somewhat hacky code with the exception as a branch
+        try {
+            const ownerId = HTMLTools.getClosestData(event, "ownerId");
+            const tokenId = HTMLTools.getClosestData(event, "tokenId");
+            const sceneId = HTMLTools.getClosestData(event, "sceneId");
+            CityHelpers.getOwner(ownerId, tokenId, sceneId);
+        }
+        catch (e) {
+            return await SceneTags.createSceneStatus();
+        }
+        return await HTMLHandlers.createStatus(event);
+    }
+    async centerOnToken(event) {
+        // const ownerId = HTMLTools.getClosestDataNT(event, "ownerId");
+        const tokenId = HTMLTools.getClosestDataNT(event, "tokenId");
+        // const sceneId = HTMLTools.getClosestDataNT(event, "sceneId");
+        if (!tokenId)
+            return;
+        const token = game.scenes.current.tokens.get(tokenId)?.object;
+        if (!token || !token.actor.isOwner)
+            return;
+        if (token.center)
+            //@ts-ignore
+            await canvas.animatePan(token.center);
+    }
+    async _dragDropEvent(event) {
+        event.stopImmediatePropagation();
+        console.debug("Standard Drag and drop");
+        const dragging = DragAndDrop.draggedElement();
+        // const existingStatus = this.getStatusAt(event);
+        let x = this.getTokenAt(event);
+        if (x == undefined) {
+            ui.notifications.error("Error with Drag and Drop");
+            return;
+        }
+        if (x instanceof Token) {
+            x = x.actor;
+        }
+        if (x == SceneTags) {
+            await DragAndDrop.dropDraggableOnSceneTags(dragging);
+            return;
+        }
+        if (x instanceof CityActor) {
+            await DragAndDrop.dropDraggableOnActor(dragging, x);
+            return;
+        }
+    }
+    async _dropOnOtherStatus(event) {
+        event.stopImmediatePropagation();
+        console.debug("Other Status Drop");
+        const dragging = DragAndDrop.draggedElement();
+        const existingStatus = this.getStatusAt(event);
+        let x = this.getTokenAt(event);
+        if (x == SceneTags) {
+            await DragAndDrop.dropDraggableOnSceneTags(dragging, { mergeStatus: existingStatus });
+            return;
+        }
+        if (x instanceof CityActor) {
+            // console.log(`existing Status: ${existingStatus?.name}`);
+            await DragAndDrop.dropDraggableOnActor(dragging, x, { mergeStatus: existingStatus });
+            return;
+        }
+    }
+    async openSheet(event) {
+        const ownerId = String(HTMLTools.getClosestDataNT(event, "ownerId"));
+        const tokenId = String(HTMLTools.getClosestDataNT(event, "tokenId"));
+        // const sceneId = HTMLTools.getClosestDataNT(event, "sceneId");
+        if (tokenId) {
+            const token = game.scenes.current.tokens.get(tokenId);
+            if (token && token.actor && token.actor.isOwner)
+                token.actor.sheet.render(true);
+            return;
+        }
+        else if (ownerId) {
+            const actor = game.actors.get(ownerId);
+            if (actor && actor.isOwner) {
+                actor.sheet.render(true);
+            }
+            return;
+        }
+    }
+}
+Hooks.on("deleteToken", async (tok) => {
+    const actor = tok.actor;
+    //fix for tokens delted that have combatants not being removed from story tag tracker
+    if (!actor) {
+        return;
+    }
+    if (game.user.isGM && game.combat) {
+        const combatant = game.combat?.getCombatantByToken(tok);
+        if (combatant) {
+            await combatant.delete();
+        }
+    }
+});
+//@ts-ignore
+window.StoryTagWindow = StoryTagWindow;
